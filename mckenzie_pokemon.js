bee7315241c047d42c846af8d887321107685234
mckenzieandrews <andrews7@stanford.edu>
2017-12-24 14:47:13 -0600

    Update mckenzie_pokemon.js

--- mckenzie_pokemon.js
+++ mckenzie_pokemon.js
@@ -1,1 +1,48 @@
 
+var Pokemon = class Pokemon {
+  constructor(String name, String type, int hp, int def, int atk, boolean legend) {
+    this.name = name;
+    this.type= type;
+    this.hp = hp;
+    this.def= def; 
+    this.atk = atk; 
+    this.legend= legend;
+  }
+  
+  
+  
+  
+  
+  get name() {
+  return this.name; 
+  }
+  
+  set name(n){
+   this.name = n; 
+  }
+   
+  get type() {
+  return this.type; 
+  }
+  
+  set type(t){
+   this.type = t; 
+  }
+   
+  get hp() {
+  return this.hp; 
+  }
+  
+  get def() {
+  return this.def; 
+  }
+  
+  get atk(){
+  return this.atk; 
+  }
+  
+  get legend(){
+  return this.legend();
+  }
+  
+};
